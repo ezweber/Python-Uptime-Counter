4b1864309f729f64e6d93b43ef668f927e92ea58
ezweber <83794833+ezweber@users.noreply.github.com>
2022-04-11 10:36:44 -0500

    Added resource monitoring.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import time
+import psutil
+import os
 
 uptime = -1
 
 while True:
+    cpu_usage = psutil.cpu_percent(2)
     uptime += 1
     print("I have been awake for:")
     print(uptime, " Minutes")
     print("{:0.2f} Hours".format(uptime/60))
+    print("Average CPU usage for last 60 seconds: ", cpu_usage)
+    print("RAM usage: ", psutil.virtual_memory()[2])
     print("<======================>")
-    time.sleep(60)
